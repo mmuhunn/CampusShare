0ab224473ebc0c66bd4639e858c7fc35a5603e3d
Baikseung <swbaik01@gmail.com>
2024-12-08 20:04:49 +0900

    Basic structure of backend project

---
+++ CampusShare_Backend/src/main/java/OSS/CampusShare_Backend/User/domain/User.java
@@ -0,0 +1,51 @@
+package OSS.CampusShare_Backend.User.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String studentId; // 학번
+
+    private String password;
+
+    // Getter와 Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
