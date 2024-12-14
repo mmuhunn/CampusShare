11908bcaa96ca037dccd3034ae2a2a0265767e48
J0725 <dudwlsjoo0725@gmail.com>
2024-12-15 03:18:42 +0900

    create API statement and modifiy some files

--- CampusShare_Backend/src/main/java/OSS/CampusShare_Backend/SwaggerConfig.java
+++ CampusShare_Backend/src/main/java/OSS/CampusShare_Backend/SwaggerConfig.java
@@ -1,2 +1,26 @@
-package OSS.CampusShare_Backend;public class SwaggerConfig {
+package OSS.CampusShare_Backend;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    Info info = new Info()
+        .title("Campus Share API")
+        .version("v1.0.0")
+        .description("Campus Share API 문서");
+
+    return new OpenAPI()
+        .info(info);
+        }
 }
